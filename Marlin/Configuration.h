6a09cb0c57e15eec516ded21c430e15e282f54b2
theHoff <dhoff1985@gmail.com>
2020-04-26 15:36:38 -0500

    Adjust z home speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1365,7 +1365,7 @@
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_Z  (2.5*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
